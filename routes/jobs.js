865b512ab86b9f7f3f269b37c856f7ec448548c0
SosukeAizen833 <Spandan833@gmai.com>
2022-01-03 14:48:41 +0530

    main image

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router();
 const {Job} = require('../models/Job');
 const User = require('../models/User');
-const mongoose = require('mongoose');
 
 //job apply route
 router.put('/user/:id/:jobId',async (req,res) => {
